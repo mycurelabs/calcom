4436c645a327ac8dafa5c6597a5e7119c1b3da02
John Leonard Tubig <tubig.jlu@gmail.com>
2024-07-03 21:47:16 +0800

    fix: fix env arg

--- start_local_db.sh
+++ start_local_db.sh
@@ -14,8 +14,8 @@ docker run \
   --rm \
   -it \
   --name calcom-postgres \
-  -env POSTGRES_USER=${POSTGRES_USER} \
-  -env POSTGRES_PASSWORD=${POSTGRES_PASSWORD} \
-  -env POSTGRES_DB=${POSTGRES_DB} \
+  -e POSTGRES_USER=${POSTGRES_USER} \
+  -e POSTGRES_PASSWORD=${POSTGRES_PASSWORD} \
+  -e POSTGRES_DB=${POSTGRES_DB} \
   -p 5432:5432 \
   postgres:16.3
